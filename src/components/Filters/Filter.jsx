694d380f4d5c6790a9713487877e7259ba2eb957
gauravsoni97 <gauravsoni8414@gmail.com>
2024-05-07 09:36:32 +0530

    filter value set from autocomplete

--- src/components/Filters/Filter.jsx
+++ src/components/Filters/Filter.jsx
@@ -2,78 +2,72 @@ import React, { useState } from "react";
 import Chip from "@mui/material/Chip";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
 import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
 
-const top100Films = [
-  { title: "The Shawshank Redemption", year: 1994 },
-  { title: "Godfather", year: 1972 },
-  { title: "Godfather", year: 1972 },
-  { title: "Godfather", year: 1972 },
-  { title: "Godfather", year: 1972 },
-  { title: "Godfather", year: 1972 },
-  { title: "Godfather", year: 1972 },
-];
-
-const CustomChip = (props) => {
-  return (
-    <Chip
-      {...props}
-      style={{ borderRadius: "4px" }}
-      deleteIcon={
-        <CloseRoundedIcon style={{ color: "black", fontSize: ".8rem" }} />
-      }
-    />
-  );
-};
+const CustomChip = (props) => (
+  <Chip
+    {...props}
+    style={{ borderRadius: "4px" }}
+    deleteIcon={<CloseRoundedIcon style={{ color: "black", fontSize: ".8rem" }} />}
+  />
+);
 
 const Filter = () => {
-  const [customFilter, setCustomFilter] = useState({
+  const [filters, setFilters] = useState({
     roles: [],
-    employeeNum: null,
     experience: null,
-    techStack: [],
     minBasePay: null,
     companyName: null,
   });
 
-  const handleAutocompleteChange = (key, value) => {
-    setCustomFilter((prevFilter) => ({
-      ...prevFilter,
-      [key]: value,
+  const handleAutocompleteChange = (filterKey, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [filterKey]: value,
     }));
   };
+  
 
+console.log("filters", filters);
   return (
-    <div
-      style={{
-        display: "flex",
-        width: "100%",
-        gap: "1rem",
-        flexWrap: "wrap",
-      }}
-    >
-      {Object.entries(customFilter).map(([key, value]) => (
-        <Autocomplete
-          sx={{ width: 180 }}
-          key={key}
-          multiple={Array.isArray(value)}
-          id={`Filter-${key}`}
-          options={top100Films}
-          getOptionLabel={(option) => option.title}
-          value={Array.isArray(value) ? value : null}
-          onChange={(event, newValue) =>
-            handleAutocompleteChange(key, newValue)
-          }
-          filterSelectedOptions
-          renderInput={(params) => <TextField {...params} placeholder={key} />}
-          renderTags={(selected, getTagProps) =>
-            selected.map((option, index) => (
-              <CustomChip label={option.title} {...getTagProps({ index })} />
-            ))
-          }
-        />
-      ))}
+    <div style={{ display: "flex", width: "100%", gap: "1rem", flexWrap: "wrap" }}>
+      <Autocomplete
+        sx={{ width: 180 }}
+        multiple
+        id="Filter-roles"
+        options={["Developer", "Designer", "Manager", "Engineer"]}
+        value={filters.roles}
+        onChange={(e, newValue) => handleAutocompleteChange("roles", newValue)}
+        filterSelectedOptions
+        renderInput={(params) => <TextField {...params} placeholder="Roles" />}
+        renderTags={(selected, getTagProps) =>
+          selected.map((option, index) => <CustomChip label={option} {...getTagProps({ index })} />)
+        }
+      />
+      <Autocomplete
+        sx={{ width: 180 }}
+        id="Filter-experience"
+        options={Array.from({ length: 10 }, (_, i) => (i + 1).toString())}
+        value={filters.experience}
+        onChange={(e, newValue) => handleAutocompleteChange("experience", newValue)}
+        renderInput={(params) => <TextField {...params} placeholder="Experience" />}
+      />
+      <Autocomplete
+        sx={{ width: 180 }}
+        id="Filter-minBasePay"
+        options={["10L", "20L", "30L", "40L", "50L", "60L", "70L"]}
+        value={filters.minBasePay}
+        onChange={(e, newValue) => handleAutocompleteChange("minBasePay", newValue)}
+        renderInput={(params) => <TextField {...params} placeholder="Min Base Pay" />}
+      />
+      <Autocomplete
+        sx={{ width: 180 }}
+        id="Filter-companyName"
+        options={["Google", "Facebook", "Amazon", "Microsoft"]}
+        value={filters.companyName}
+        onChange={(e, newValue) => handleAutocompleteChange("companyName", newValue)}
+        renderInput={(params) => <TextField {...params} placeholder="Company Name" />}
+      />
     </div>
   );
 };
